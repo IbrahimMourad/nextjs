1c4c16699fe16c04d1c506d1e76cb381003fa44b
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-09-23 11:50:23 +0200

    init commit

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="Ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius animi
+          vel illo architecto quam dolor deserunt tempora repellat sit,
+          veritatis eaque! Laudantium quia blanditiis perferendis temporibus
+          necessitatibus amet debitis incidunt!
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
